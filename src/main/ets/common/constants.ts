a9333f5aa14562837ea9942dff3e606d60be16b5
ccccjiemo <631003561@qq.com>
2024-12-26 20:22:14 +0800

    command queue

--- src/main/ets/common/constants.ts
+++ src/main/ets/common/constants.ts
@@ -1,5 +1,11 @@
-
 export const DOMAIN = 0xffff;
 
 export const TAG = "GLView";
 
+export const GL_Command_Queue_Domain = 0xf1;
+
+export const GL_Command_Queue_Tag = "GLCommandQueue";
+
+export const GL_Thread_Domain = 0xf0;
+
+export const GL_Thread_Tag = "GLThread"
